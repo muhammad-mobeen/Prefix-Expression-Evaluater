652baab89dec5c4168f2dc04c0987c66f44569d7
Muhammad Mobeen <70343951+muhammad-mobeen@users.noreply.github.com>
2021-10-19 00:18:02 +0500

    Pycharm Charmness!

--- main.py
+++ main.py
@@ -22,6 +22,36 @@ class Stack:
 
     def size(self):
         return len(self.control)
-    
+
     def is_empty(self):
         return len(self.control) == 0
+
+
+def evaluator(exp):
+    express = Stack()
+    eva = Stack()
+    for c in exp:
+        express.push(c)
+
+    for x in range(express.size()):
+        popped = express.pop()
+        if popped.isalpha() or popped in '1234567890':
+            eva.push(popped)
+        elif popped in '+-*/':
+            op1 = int(eva.pop())
+            op2 = int(eva.pop())
+            if popped == '+':
+                eva.push(op2 + op1)
+            elif popped == '-':
+                eva.push(op2 - op1)
+            elif popped == '*':
+                eva.push(op2 * op1)
+            else:
+                eva.push(op2 / op1)
+    return eva.top()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    s = "-+7*45+20"
+    print(evaluator(s))
